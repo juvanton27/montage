ea73fdf5a0acb9c57a1c3ba75ff2f8d299c1e9bc
Julien Van Tongerloo <julien.vantongerloo@intuitim.com>
2022-08-07 20:31:02 +0200

    fix - gitignore

--- backend/dist/services/videos/videos.service.d.ts
+++
@@ -1,4 +0,0 @@
-import { Inputs } from 'src/controllers/videos/videos.controller';
-export declare class VideosService {
-    createVideo(inputs: Inputs): void;
-}
--- backend/dist/utils/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function ffmpegDurationToSeconds(duration: string): number;
-export declare function sleep(ms: any): Promise<unknown>;
